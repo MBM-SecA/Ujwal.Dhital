2b9b6e728542a2c9afbf4ad8f9cd2cfe195c0802
uzwaldhital <uzwaldhital>
2020-09-24 09:34:34 +0545

    Assignment2completed

--- Assign1/Directories.cs
+++ Assign1/Directories.cs
@@ -1,28 +1,28 @@
-
 //Create a folder name "ABC" and create 10 sub folder where each folder should contain C# with a program to print Hello World.
 using System;
+using System.IO;
 
-using System.IO; 
- 
 public class CreateDirExecute
 {
- 
-internal bool CreateDir(){
-Directory.CreateDirectory(@"D:\6th sem\DOTnet\Ujwal.Dhital\Assign1\ABC");
-return true;
-}
- 
-internal bool CreateSubDir()
-{
-bool res = CreateDir();
-if(res){
-for(int i=1;i<=10;i++){
-Directory.CreateDirectory($"ABC/abc{i}");
-string programSource = "using System;namespace Assignment_Q { class PrintHelloWorld {void Display(){ Console.WriteLine(\"Hello world\");}}}";
-File.WriteAllText($"ABC/abc{i}/program.cs",programSource);
-}
-}
-return true;
-}
-}
 
+    internal bool CreateDir () 
+    {
+        Directory.CreateDirectory (@"D:\6th sem\DOTnet\Ujwal.Dhital\Assign1\ABC");
+        return true;
+    }
+
+    internal bool CreateSubDir ()
+    {
+        bool res = CreateDir ();
+        if (res)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Directory.CreateDirectory ($"ABC/abc{i}");
+                string programSource = "using System;namespace Assignment_Q { class PrintHelloWorld {void Display(){ Console.WriteLine(\"Hello world\");}}}";
+                File.WriteAllText ($"ABC/abc{i}/program.cs", programSource);
+            }
+        }
+        return true;
+    }
+}
--- Assign1/Program.cs
+++ Assign1/Program.cs
@@ -1,14 +1,16 @@
 ﻿//All the assignment1 question
- 
-using System;
-class Assignment1 {
- static void Main()
- {
 
- CreateDirExecute obj1 = new CreateDirExecute();
- bool res = obj1.CreateSubDir();
- if (res){
- Console.WriteLine("Directory and sub-directory created and added program source inside it");
- }
- }
+using System;
+class Assignment1 
+{
+    static void Main ()
+    {
+
+        CreateDirExecute obj1 = new CreateDirExecute ();
+        bool res = obj1.CreateSubDir ();
+        if (res) 
+        {
+            Console.WriteLine ("Directory and sub-directory created and added program source inside it");
+        }
+    }
 }
